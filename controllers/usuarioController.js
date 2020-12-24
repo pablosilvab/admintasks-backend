f8afa93382620ffaddef2e849455e8149d11ec60
Pablo Silva <pablonicolassilvabravo@gmail.com>
2020-12-24 11:55:16 -0300

    hash password

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,4 +1,5 @@
 const Usuario = require("../models/Usuario");
+const bcryptjs = require('bcryptjs')
 
 exports.crearUsuario = async (req, res) => {
   const { email, password } = req.body;
@@ -12,6 +13,10 @@ exports.crearUsuario = async (req, res) => {
       });
     }
     usuario = new Usuario(req.body);
+    
+    const salt = await bcryptjs.genSalt(10)
+    usuario.password = await bcryptjs.hash(password, salt)
+    
     await usuario.save();
     res.status(201).json({
       msg: "Usuario creado correctamente",
