fa1d027eeb2f7b35292eae5278c0360f0e2a6aeb
Pablo Silva <pablonicolassilvabravo@gmail.com>
2020-12-30 12:01:52 -0300

    actualizacion y eliminacion de proyectos.

--- controllers/proyectoController.js
+++ controllers/proyectoController.js
@@ -4,17 +4,92 @@ const { validationResult } = require("express-validator");
 const jwt = require("jsonwebtoken");
 
 exports.crearProyecto = async (req, res) => {
+  const errores = validationResult(req);
+
+  if (!errores.isEmpty()) {
+    return res.status(400).json({
+      errores: errores.array(),
+    });
+  }
+
   try {
     const proyecto = new Proyecto(req.body);
 
     // jwt
-    proyecto.creador = req.usuario.id
+    proyecto.creador = req.usuario.id;
 
     proyecto.save();
     res.status(201).json({
       msg: "Proyecto ingresado correctamente",
       data: proyecto,
     });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Hubo un error");
+  }
+};
+
+exports.obtenerProyectos = async (req, res) => {
+  try {
+    const proyectos = await Proyecto.find({ creador: req.usuario.id }).sort({
+      fechaCreacion: -1,
+    });
+    res.json({ proyectos });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Hubo un error");
+  }
+};
+
+exports.actualizarProyecto = async (req, res) => {
+  const errores = validationResult(req);
+
+  if (!errores.isEmpty()) {
+    return res.status(400).json({
+      errores: errores.array(),
+    });
+  }
+
+  const { nombre } = req.body;
+  const nuevoProyecto = {};
+
+  if (nombre) {
+    nuevoProyecto.nombre = nombre;
+  }
+
+  try {
+    let proyecto = await Proyecto.findById(req.params.id);
+    if (!proyecto) {
+      return res.status(404).json({ msg: "Proyecto no existe" });
+    }
+    if (proyecto.creador.toString() !== req.usuario.id) {
+      return res.status(401).json({ msg: "Usuario no autorizado" });
+    }
+    proyecto = await Proyecto.findByIdAndUpdate(
+      { _id: req.params.id },
+      { $set: nuevoProyecto },
+      { new: true }
+    );
+
+    res.json({ proyecto });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Hubo un error");
+  }
+};
+
+exports.eliminarProyecto = async (req, res) => {
+  try {
+    let proyecto = await Proyecto.findById(req.params.id);
+    if (!proyecto) {
+      return res.status(404).json({ msg: "Proyecto no existe" });
+    }
+    if (proyecto.creador.toString() !== req.usuario.id) {
+      return res.status(401).json({ msg: "Usuario no autorizado" });
+    }
+
+    await Proyecto.findByIdAndRemove({ _id: req.params.id });
+    res.json({ msg: "Proyecto eliminado correctamente" });
   } catch (error) {
     console.log(error);
     res.status(500).send("Hubo un error");
--- routes/proyectos.js
+++ routes/proyectos.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const router = express.Router();
-const { check } = require("express-validator");
 const proyectoController = require("../controllers/proyectoController");
 const auth = require("../middleware/auth");
+const { check } = require("express-validator");
 
-router.post("/", auth, proyectoController.crearProyecto);
-router.get("/", auth, proyectoController.crearProyecto);
+router.post(
+  "/",
+  auth,
+  [check("nombre", "El nombre del proyecto es obligatorio").not().isEmpty()],
+  proyectoController.crearProyecto
+);
+router.get("/", auth, proyectoController.obtenerProyectos);
+router.put(
+  "/:id",
+  auth,
+  [check("nombre", "El nombre del proyecto es obligatorio").not().isEmpty()],
+  proyectoController.actualizarProyecto
+);
+router.delete("/:id", auth, proyectoController.eliminarProyecto);
 
 module.exports = router;
